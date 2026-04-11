ae68f5ffd3ada485adeece072b2090a3ff703754
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2026-04-11 15:35:07 -0400

    Updated main menu scene

---
+++ Assets/Scripts/UI/ButtonHandler.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "ButtonHandler", menuName = "Scriptable Objects/ButtonHandler")]
+public class ButtonHandler : ScriptableObject
+{
+    public void OnClick_PlayGame()
+    {
+        SceneManager.LoadScene("MapScene");
+    }
+}
